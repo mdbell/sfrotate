4ccd8ca9c2849e54e55a10d4cbd9624b398ecedb
Matt <github@mdbell.me>
2025-09-14 19:55:44 +0000

    DIY injector now

---
+++ inject.sh
@@ -0,0 +1,4 @@
+cp /sdcard/sfrotate/* /data/local/tmp/
+chmod 755 /data/local/tmp/dlopen64
+chmod 755 /data/local/tmp/libsf_rotate.so
+/data/local/tmp/dlopen64 surfaceflinger /data/local/tmp/libsf_rotate.so
