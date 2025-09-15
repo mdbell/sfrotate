54b02813fa8f69887b4993aeb485f7fc5ff1e503
Matt <github@mdbell.me>
2025-09-15 19:02:41 +0000

    removed unused func from header file

--- src/gnu_debugdata_resolver.h
+++ src/gnu_debugdata_resolver.h
@@ -4,4 +4,3 @@
 uintptr_t resolve_addr_from_gnu_debugdata(const char* exe_path,
                                           const char* mangled_name,
                                           uintptr_t runtime_base);
-uintptr_t get_sf_runtime_base();
